9832445daf09b429ec9d32710b39f9f804323634
jnnr <32454596+jnnr@users.noreply.github.com>
2020-10-12 18:23:43 +0200

    Add infer and optimize script

---
+++ examples/simple_model/optimize.py
@@ -0,0 +1,37 @@
+import logging
+import os
+
+from oemof.solph import EnergySystem, Model
+
+# DONT REMOVE THIS LINE!
+# pylint: disable=unused-import
+from oemof.tabular import datapackage  # noqa
+from oemoflex.facades import TYPEMAP
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+preprocessed = os.path.join(here, 'preprocessed')
+
+optimized = os.path.join(here, 'optimized')
+
+if not os.path.exists(optimized):
+    os.mkdir(optimized)
+
+es = EnergySystem.from_datapackage(
+    os.path.join(preprocessed, "datapackage.json"),
+    attributemap={}, typemap=TYPEMAP,
+)
+
+# create model from energy system (this is just oemof.solph)
+m = Model(es)
+
+# select solver 'gurobi', 'cplex', 'glpk' etc
+m.solve(solver='cbc')
+
+# get the results from the the solved model(still oemof.solph)
+es.results = m.results()
+
+# now we use the write results method to write the results in oemof-tabular
+# format
+es.dump(optimized)
