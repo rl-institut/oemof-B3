195e2d12314919a8fb1a5aacfe662b3a01bd9d10
jnnr <32454596+jnnr@users.noreply.github.com>
2020-10-13 10:40:50 +0200

    Correct project name

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'rli_template'
+project = 'oemof-B3'
 copyright = '2020, Reiner Lemoine Institut'
 author = 'Reiner Lemoine Institut'
 
