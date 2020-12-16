a1a6c25d68e346580779cb0c7b79f87822d038a1
caroline.moeller <caroline.moeller@rl-institut.de>
2020-12-16 11:11:39 +0100

    Add the other both models

---
+++ examples/simple_model_2/optimize.py
@@ -0,0 +1,39 @@
+import logging
+import os
+
+from oemof.solph import EnergySystem, Model
+
+# DONT REMOVE THIS LINE!
+# pylint: disable=unused-import
+from oemof.tabular import datapackage  # noqa
+from oemof.tabular.facades import TYPEMAP
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+name = 'simple_model_2'
+
+preprocessed = os.path.join(here, 'preprocessed', name)
+
+optimized = os.path.join(here, 'optimized')
+
+if not os.path.exists(optimized):
+    os.mkdir(optimized)
+
+es = EnergySystem.from_datapackage(
+    os.path.join(preprocessed, "datapackage.json"),
+    attributemap={}, typemap=TYPEMAP,
+)
+
+# create model from energy system (this is just oemof.solph)
+m = Model(es)
+
+# select solver 'gurobi', 'cplex', 'glpk' etc
+m.solve(solver='cbc')
+
+# get the results from the the solved model(still oemof.solph)
+es.results = m.results()
+
+# now we use the write results method to write the results in oemof-tabular
+# format
+es.dump(optimized)
---
+++ examples/simple_model_2/setup_model_structure.py
@@ -0,0 +1,29 @@
+import os
+
+from oemofB3.model_structure import create_default_data
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+preprocessed_elements = os.path.join(here, 'preprocessed', 'simple_model', 'data')
+
+if not os.path.exists(preprocessed_elements):
+    os.makedirs(preprocessed_elements)
+
+create_default_data(
+    preprocessed_elements,
+    select_components=[
+        'electricity-shortage',
+        'electricity-curtailment',
+        'electricity-demand',
+        'electricity-transmission',
+        'wind-onshore',
+        'solar-pv',
+        'ch4-gt',
+        'oil-st',
+        'other-st',
+        'electricity-liion_battery',
+        'biomass-st',
+    ],
+    dummy_sequences=True,
+)
