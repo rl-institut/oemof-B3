4fd8e369d16f1c7f6b6131f3ec27c63eaf32104b
jnnr <32454596+jnnr@users.noreply.github.com>
2021-02-02 13:51:57 +0100

    Add script for optimization

---
+++ oemof_b3/scripts/optimize.py
@@ -0,0 +1,40 @@
+import logging
+import os
+import sys
+
+from oemof.solph import EnergySystem, Model
+
+# DONT REMOVE THIS LINE!
+# pylint: disable=unusedimport
+from oemof.tabular import datapackage  # noqa
+from oemof.tabular.facades import TYPEMAP
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+name = 'simple_model'
+
+preprocessed = sys.argv[1]
+
+optimized = sys.argv[2]
+
+if not os.path.exists(optimized):
+    os.mkdir(optimized)
+
+es = EnergySystem.from_datapackage(
+    os.path.join(preprocessed, "datapackage.json"),
+    attributemap={}, typemap=TYPEMAP,
+)
+
+# create model from energy system (this is just oemof.solph)
+m = Model(es)
+
+# select solver 'gurobi', 'cplex', 'glpk' etc
+m.solve(solver='cbc')
+
+# get the results from the the solved model(still oemof.solph)
+es.results = m.results()
+
+# now we use the write results method to write the results in oemoftabular
+# format
+es.dump(optimized)
