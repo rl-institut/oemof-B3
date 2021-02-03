403cda679a0f46908d25057182d9e859f23b6b13
jnnr <32454596+jnnr@users.noreply.github.com>
2021-02-03 21:08:43 +0100

    Sketch using frictionless

--- examples/variations/create_variations.py
+++ examples/variations/create_variations.py
@@ -1,6 +1,6 @@
 import sys
 
-from datapackage import DataPackage
+from frictionless import Package
 import pandas as pd
 
 from oemofB3.variations import VariationGen
@@ -10,10 +10,10 @@ dp_path = sys.argv[1]
 
 destination = sys.argv[2]
 
-variations = pd.read_csv('efficiency_variations.csv')
+variations = pd.read_csv('efficiency_variations.csv', header=[0,1], index_col=0)
 
 # load datapackage
-dp = DataPackage(dp_path)
+dp = Package(dp_path)
 
 # setup variation generator
 vg = VariationGen(dp)
--- oemofB3/variations.py
+++ oemofB3/variations.py
@@ -1,4 +1,4 @@
-from datapackage import DataPackage
+from frictionless import steps, transform
 
 
 class VariationGen:
@@ -7,19 +7,41 @@ class VariationGen:
 
         self.datapackage = dp
 
-        print(dp.resources)
-
     def create_variations(self, variations, destination):
 
         for id, row in variations.iterrows():
 
-            _dp = self.dp.copy()
+            _dp = self.datapackage.to_copy()
 
-            self.set_values(_dp, row)
+            _dp = self.set_values(_dp, row)
+            print('saving')
+            _dp.to_json('~/Desktop/pack.json')
 
     @staticmethod
     def set_values(_dp, row):
-        pass
+        from pprint import pprint
+        # evtl https: // github.com / frictionlessdata / datapackage - pipelines - pandas - driver
+        element, variable = row.index.item()
 
+        value = row.item()
+
+        print(element)
+        print(variable)
+        print(value)
+
+        e = _dp.get_resource(element)
+        print(e.name)
+        dd = e.read_data()
+
+        # https://frictionlessdata.io/tooling/python/transforming-data/#set-cells
+        e = transform(
+            e,
+            steps=[
+                steps.cell_set(field_name=variable, value=100),
+            ]
+        )
+        dd = e.read_data()
+
+        return _dp
 
 
