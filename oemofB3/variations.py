430ba756a53ff0c9b4765e6776a508557e076854
jnnr <32454596+jnnr@users.noreply.github.com>
2021-02-15 13:00:53 +0100

    Comment and clarify api

--- oemofB3/variations.py
+++ oemofB3/variations.py
@@ -63,29 +63,37 @@ class DataDict:
 
     @classmethod
     def from_csv_dir(cls, dir):
+        r"""
+        Initialize a DataDict from a csv directory
+        """
+        rel_paths = cls._get_rel_paths(dir, '.csv')
 
-        rel_paths = cls.get_rel_paths(dir, '.csv')
-
-        data = cls.load_csv(cls, dir, rel_paths)
+        data = cls._load_csv(cls, dir, rel_paths)
 
         return cls(dir, data, rel_paths)
 
     @classmethod
     def from_metadata(cls, json_file_path):
-
+        r"""
+        Initialize a DataDict from the metadata string,
+        usually named datapackage.json
+        """
         dp = Package(json_file_path)
 
         dir = os.path.split(json_file_path)[0]
 
         rel_paths = {r['name']: r['path'] for r in dp.resources}
 
-        data = cls.load_csv(cls, dir, rel_paths)
+        data = cls._load_csv(cls, dir, rel_paths)
 
         return cls(dir, data, rel_paths)
 
     @staticmethod
-    def get_rel_paths(dir, file_ext):
-
+    def _get_rel_paths(dir, file_ext):
+        r"""
+        Get paths to all files in a given directory relative
+        to the root with a given file extension.
+        """
         rel_paths = {}
         for root, dirs, files in os.walk(dir):
 
@@ -98,28 +106,33 @@ class DataDict:
 
         return rel_paths
 
-    def load_csv(self, basepath, rel_paths):
+    def _load_csv(self, basepath, rel_paths):
+        r"""
+        Load a DataDict from csv files.
+        """
         data = {}
 
         for name, path in rel_paths.items():
             full_path = os.path.join(basepath, path)
-            data[name] = self.read_resource(full_path)
+            data[name] = self._read_resource(full_path)
 
         return data
 
-    def save_csv(self, destination):
-
+    def to_csv_dir(self, destination):
+        r"""
+        Save the DataDict to csv files.
+        """
         for name, data in self.data.items():
             path = self.rel_paths[name]
             full_path = os.path.join(destination, path)
-            self.write_resource(data, full_path)
+            self._write_resource(data, full_path)
 
     @staticmethod
-    def read_resource(path):
+    def _read_resource(path):
         return pd.read_csv(path)
 
     @staticmethod
-    def write_resource(data, path):
+    def _write_resource(data, path):
         root = os.path.split(path)[0]
 
         if not os.path.exists(root):
