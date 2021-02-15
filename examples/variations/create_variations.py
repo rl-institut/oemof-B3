756ed50473dc58a8c020dd75049f207e9d9b2995
jnnr <32454596+jnnr@users.noreply.github.com>
2021-02-15 14:53:33 +0100

    Update example script

--- examples/variations/create_variations.py
+++ examples/variations/create_variations.py
@@ -1,10 +1,8 @@
 import sys
 
-from frictionless import Package
 import pandas as pd
-
-from oemofB3.variations import VariationGen
-
+from frictionless import Package
+from oemofB3.variations import DataFramePackage, VariationGenerator
 
 dp_path = sys.argv[1]
 
@@ -13,10 +11,10 @@ destination = sys.argv[2]
 variations = pd.read_csv('efficiency_variations.csv', header=[0,1], index_col=0)
 
 # load datapackage
-dp = Package(dp_path)
+dp = DataFramePackage.from_csv_dir(dp_path)
 
 # setup variation generator
-vg = VariationGen(dp)
+vg = VariationGenerator(dp)
 
 # create variations
 vg.create_variations(variations, destination)
