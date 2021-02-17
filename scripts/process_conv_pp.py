c5fcfe7ed990bc4931c7e178a1349228d9dbdde9
Julius Meier <julius.meier@gmx.de>
2021-02-17 17:00:32 +0100

    Change input and output path mechanism and remove utf8 comment

--- scripts/process_conv_pp.py
+++ scripts/process_conv_pp.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 12 10:40:09 2021
-
-@author: Julius
-"""
-
 import sys
 import pandas as pd
 
-in_path = sys.argv[1] + '\conventional_power_plants_DE.csv'
-out_path = sys.argv[2] + '\processed_conv_pp.csv'
+in_path = sys.argv[1]
+out_path = sys.argv[2]
 
 if __name__ == '__main__':
     opsd = pd.read_csv(in_path)
