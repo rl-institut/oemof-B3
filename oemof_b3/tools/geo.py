e7c6ef4eb901949763e347bd8406fa31f6f0f45d
Julius Meier <julius.meier@gmx.de>
2021-03-18 09:49:11 +0100

    move auxiliary functions to tools folder

--- oemof_b3/tools/geo.py
+++ oemof_b3/tools/geo.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
-from tabulate import tabulate
-
-opsd = pd.read_csv (r'C:\Users\meinm\Documents\Git\oemof-B3\raw\conventional_power_plants_DE.csv')
-
-b3 = opsd[opsd.state.isin(['Brandenburg', 'Berlin'])]
-b3 = b3.copy()
-b3.reset_index(inplace=True, drop=True)
-#print(tabulate(b3, headers='keys', tablefmt='psql'))
 
 
+def load_regions_file(file_path):
+    """
+    loads a geopackage containing all regions of Germany and filters for geodata of Berlin and
+    all regions in Brandenburg
 
-
-
-def load_regions_file():
+    Outputs
+    --------------
+    geopandas.GeoDataFrame
+        with geoinformation of Berlin and all regions in Brandenburg
+    """
     file_path = r"C:\Users\meinm\Documents\Git\oemof-B3\raw\boundaries_germany_nuts3.gpkg"
     de = gpd.read_file(file_path)
 
@@ -54,44 +52,25 @@ def load_regions_file():
 def add_region_to_register(register, regions):
     """
     adds the region to a power plant
+
     Input
     ---------------
     'register': pandas.DataFrame
         with columns lat, lon
     'region': geopandas.GeoDataFrame
         with rows for the specific regions
-    returns
+
+    Outputs
     --------------
     pandas.DataFrame
-        with new column containing the region name
+        with new column 'name' containing the region name
     """
     # transform the lat/lon coordinates into a shapely point coordinates and
     # add column named "coordinates"
     register['coordinates'] = list(zip(register.lon, register.lat))
     register['coordinates'] = register['coordinates'].apply(Point)
     register_gdf = gpd.GeoDataFrame(register, geometry='coordinates', crs=4326)
-    #regions_gdf = gpd.GeoDataFrame(regions, geometry='geometry')
-    new_register = gpd.sjoin(register_gdf, regions, op='within')
+    new_register_gdf = gpd.sjoin(register_gdf, regions, op='within')
+    new_register = pd.DataFrame(new_register_gdf)
 
-    new_register.drop(columns=['index_right', 'id_right', 'type_right', 'nuts', 'state_id'], inplace=True)
-    new_register.rename(columns={'id_left': 'id', 'name': 'region'}, inplace=True)
-
-    return(new_register)
-
-bbb = load_regions_file()
-new_b3 = add_region_to_register(b3, bbb)
-
-print(tabulate(new_b3, headers='keys', tablefmt='psql'))
-print(new_b3.shape)
-
-
-a = new_b3.groupby('region')
-print(tabulate(a, headers='keys', tablefmt='psql'))
-
-b = a.agg({'capacity_net_bnetza':'sum'})
-print(tabulate(b, headers='keys', tablefmt='psql'))
-
-print(type(a), type(b))
-
-def regional_aggregate(df):
-    print('Hallo')
+    return new_register
--- scripts/prepare_conv_pp.py
+++ scripts/prepare_conv_pp.py
@@ -19,12 +19,42 @@ saved as a csv file.
 import sys
 import pandas as pd
 
-in_path = sys.argv[1]
-out_path = sys.argv[2]
+from os.path import dirname, abspath
+d = dirname(dirname(abspath(__file__)))
+
+sys.path.append(d)
+
+import oemof_b3.tools.geo as geo
+
+
+in_path = sys.argv[1] # path to OPSD data
+in_path2 = sys.argv[2] # path to geopackage of german regions
+out_path = sys.argv[3]
 
 if __name__ == "__main__":
     opsd = pd.read_csv(in_path)
     b3 = opsd[opsd.state.isin(['Brandenburg', 'Berlin'])]
     b3 = b3.copy()
     b3.reset_index(inplace=True, drop=True)
-    b3.to_csv(out_path, index=False)
+
+    b3_regions = geo.load_regions_file(in_path2)
+    b3 = geo.add_region_to_register(b3, b3_regions)
+
+    # clean up table columns
+    b3.drop(b3.loc[b3['status']=='shutdown'].index, inplace = True)
+    b3.drop(b3.loc[b3['status'] == 'shutdown_temporary'].index, inplace=True)
+    b3.drop(columns=['index_right', 'id_right', 'type_right', 'nuts', 'state_id',
+                     'name_bnetza', 'block_bnetza', 'capacity_gross_uba',
+                     'name_uba', 'company', 'street', 'postcode', 'city', 'state',
+                     'country', 'type_left', 'lat', 'lon', 'eic_code_plant',
+                     'eic_code_block', 'network_node', 'voltage', 'network_operator',
+                     'efficiency_source', 'shutdown', 'retrofit', 'efficiency_data',
+                     'efficiency_estimate', 'commissioned_original', 'energy_source_level_1',
+                     'energy_source_level_2', 'energy_source_level_3',
+                     'merge_comment', 'comment', 'coordinates'], inplace=True)
+    b3.rename(columns={'name': 'region', 'capacity_net_bnetza': 'capacity_net'}, inplace=True)
+
+    # aggregate
+
+    b3.to_excel(out_path, index=False)
+    #b3.to_csv(out_path, index=False)
