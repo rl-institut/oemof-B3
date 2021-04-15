6a91d962a18e33fa5f34ea8d151af6d0a477608c
jnnr <32454596+jnnr@users.noreply.github.com>
2021-04-15 16:46:48 +0200

    Setup default datapackage from scenario data

--- scripts/build_datapackage.py
+++ scripts/build_datapackage.py
@@ -1,11 +1,6 @@
-import os
 import sys
 
 import pandas as pd
-# DONT REMOVE THIS LINE!
-# pylint: disable=unused-import
-from oemof.tabular import datapackage  # noqa
-from oemof.tabular.facades import TYPEMAP
 from oemoflex.model.datapackage import EnergyDataPackage
 from oemoflex.tools.helpers import load_yaml
 
@@ -18,39 +13,15 @@ if __name__ == "__main__":
 
     datetimeindex = (pd.date_range(start="2019-01-01", freq="H", periods=8760),)
 
-    print(scenario_specs)
-    keys = ["busses", "select_components", "select_regions", "select_links"]
-
-    extra_kwargs = {}
-    for key in keys:
-        if key in scenario_specs:
-            extra_kwargs[key] = scenario_specs[key]
-
-
     # setup default structure
     edp = EnergyDataPackage.setup_default(
-        # TODO: Pass scenario specs.
-        name='simple_model',
-        basepath=preprocessed,
+        basepath=destination,
         datetimeindex=pd.date_range("1/1/2016", periods=24 * 10, freq="H"),
-        components=[
-            'ch4-boiler',
-            'heat-demand'
-        ],
-        busses=[
-            'ch4',
-            'heat',
-        ],
-        regions=['A', 'B'],
-        links=['A-B'],
+        **scenario_specs,
     )
 
     # save to csv
-    edp.to_csv_dir(preprocessed)
+    edp.to_csv_dir(destination)
 
     # add metadata
     edp.infer_metadata()
-
-    create_default_data(
-        destination=destination, datetimeindex=datetimeindex, **extra_kwargs
-    )
