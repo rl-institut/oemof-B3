be3bdc77e97e42b204a9e61915802a6e22479fd4
unknown <julius.meier@gmx.de>
2021-05-17 11:12:39 +0200

    Plot one winter and one summer month in plot_dispatch.py

--- scripts/plot_dispatch.py
+++ scripts/plot_dispatch.py
@@ -21,29 +21,31 @@ if __name__ == "__main__":
 
             data = pd.read_csv(electricity_bus_path, header=[0, 1, 2], parse_dates=[0], index_col=[0])
 
-            fig, ax = plt.subplots(figsize=(12, 5))
-            data = plots.eng_format(ax, data, 'W', 1000)
+            # plot one winter and one summer month
+            for i in ["01", "07"]:
+                fig, ax = plt.subplots(figsize=(12, 5))
+                data = plots.eng_format(ax, data, 'W', 1000)
 
-            start_date = '2019-12-01 00:00:00'
-            end_date = '2019-12-13 23:00:00'
-            plots.plot_dispatch(ax=ax, df=data, start_date=start_date, end_date=end_date,
-                                bus_name=electricity_bus_name)
+                start_date = '2019-' + i + '-01 00:00:00'
+                end_date = '2019-' + i + '-31 23:00:00'
+                plots.plot_dispatch(ax=ax, df=data, start_date=start_date, end_date=end_date,
+                                    bus_name=electricity_bus_name)
 
-            plt.grid()
-            plt.title(electricity_bus_name + " Dispatch", pad=20, fontdict={'size':22})
-            plt.xlabel("Date", loc='right', fontdict={'size':17})
-            plt.ylabel("Power", loc='top', fontdict={'size':17})
-            plt.xticks(fontsize=14)
-            plt.yticks(fontsize=14)
- 
-            # Shrink current axis's height by 10% on the bottom
-            box = ax.get_position()
-            ax.set_position([box.x0, box.y0 + box.height * 0.15,
-                             box.width, box.height * 0.85])
-            # Put a legend below current axis
-            ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
-                      fancybox=True, ncol=5, fontsize=14)
+                plt.grid()
+                plt.title(electricity_bus_name + " Dispatch", pad=20, fontdict={'size': 22})
+                plt.xlabel("Date", loc='right', fontdict={'size': 17})
+                plt.ylabel("Power", loc='top', fontdict={'size': 17})
+                plt.xticks(fontsize=14)
+                plt.yticks(fontsize=14)
 
-            fig.tight_layout()
-            file_name = electricity_bus_name + ".pdf"
-            plt.savefig(os.path.join(plotted, file_name), bbox_inches='tight')
+                # Shrink current axis's height by 10% on the bottom
+                box = ax.get_position()
+                ax.set_position([box.x0, box.y0 + box.height * 0.15,
+                                 box.width, box.height * 0.85])
+                # Put a legend below current axis
+                ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+                          fancybox=True, ncol=5, fontsize=14)
+
+                fig.tight_layout()
+                file_name = electricity_bus_name + " " + start_date[5:7] + ".pdf"
+                plt.savefig(os.path.join(plotted, file_name), bbox_inches='tight')
