6565e3cf0ce24d00892080d039f65a165364f80e
jnnr <32454596+jnnr@users.noreply.github.com>
2021-05-31 15:12:38 +0200

    Update example scripts

---
+++ examples/setup_model_structure.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+import pandas as pd
+
+from oemoflex.model.datapackage import EnergyDataPackage
+
+# select scenario
+scenario_names = [
+    'simple_model',
+    'simple_model_1',
+    'simple_model_2',
+]
+
+try:
+    scenario_name = sys.argv[1]
+
+except IndexError:
+    raise IndexError("Please pass scenario name.")
+
+assert scenario_name in scenario_names, f"Scenario '{scenario_name}' not found. Please select one" \
+                                        f" of the defined scenarios ({scenario_names})."
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+preprocessed = os.path.join(here, scenario_name, "preprocessed", scenario_name)
+
+if not os.path.exists(preprocessed):
+    os.makedirs(preprocessed)
+
+# setup default structure
+edp = EnergyDataPackage.setup_default(
+    name=scenario_name,
+    basepath=preprocessed,
+    datetimeindex=pd.date_range("1/1/2016", periods=24 * 10, freq="H"),
+    regions=['BB', 'BE'],
+    links=['BB-BE'],
+    busses=['electricity', 'ch4'],
+    components=[
+        "electricity-shortage",
+        "electricity-curtailment",
+        "electricity-demand",
+        "electricity-transmission",
+        "wind-onshore",
+        "solar-pv",
+        "ch4-gt",
+        # "oil-st",
+        # "other-st",
+        "electricity-liion_battery",
+        # "biomass-st",
+    ],
+)
+
+# save to csv
+edp.to_csv_dir(preprocessed)
+
+# add metadata
+edp.infer_metadata()
