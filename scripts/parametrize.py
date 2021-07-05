4bb90c3a0eb840febfaf2c692dfdf3185de76e2c
jnnr <32454596+jnnr@users.noreply.github.com>
2021-07-05 20:18:00 +0200

    Stack, parametrize, unstack

--- scripts/parametrize.py
+++ scripts/parametrize.py
@@ -1,7 +1,20 @@
 import sys
+import pandas as pd
 
 from oemoflex.model.datapackage import EnergyDataPackage
 from oemoflex.tools.helpers import load_yaml
+
+
+def update_with_checks(old, new):
+    # Check if some data would get lost
+    if not new.index.isin(old.index).all():
+        raise Warning("Index of new data is not in the index of old data.")
+
+    try:
+        # Check if it overwrites by setting errors = 'raise'
+        old.update(new, errors="raise")
+    except ValueError:
+        raise Warning("Update would overwrite existing data.")
 
 
 if __name__ == "__main__":
@@ -14,9 +27,16 @@ if __name__ == "__main__":
     # setup default structure
     edp = EnergyDataPackage.from_csv_dir(edp_path)
 
-    edp.parametrize(frame='heat-demand', column='amount', values=1)
+    # Stack
+    edp.stack_components()
 
-    edp.parametrize(frame='heat-demand_profile', column='BB-heat-demand-profile', values=[0] * 240)
+    path_scalars = scenario_specs["path_scalars"]
+
+    scalars = pd.read_csv(path_scalars, index_col=[0, 1])["var_value"]
+
+    update_with_checks(edp.data["component"], scalars)
+
+    edp.unstack_components()
 
     # save to csv
     edp.to_csv_dir(edp_path)
