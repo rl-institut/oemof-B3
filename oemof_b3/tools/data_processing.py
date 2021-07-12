6ebba7a730c458854d68b1247dca33f73d52ba46
jnnr <32454596+jnnr@users.noreply.github.com>
2021-07-12 16:51:03 +0200

    Add tests and dummy functions for timeseries stack/unstack

---
+++ oemof_b3/tools/data_processing.py
@@ -0,0 +1,18 @@
+def stack_timeseries(df):
+    _df = df.copy()
+
+    # assert that df has a timeindex
+
+    # stack timeseries
+    df_stacked = _df
+
+    return df_stacked
+
+
+def unstack_timeseries(df):
+    _df = df.copy()
+
+    # stack timeseries
+    df_unstacked = _df
+
+    return df_unstacked
---
+++ tests/test_data_processing.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+from oemof_b3.tools.data_processing import stack_timeseries, unstack_timeseries
+
+ts_row_wise_cols = [
+    "var_name",
+    "timeindex_start",
+    "timeindex_stop",
+    "timeindex_resolution",
+    "series",
+]
+
+ts_column_wise = pd.DataFrame(
+    np.random.randint(0, 10, size=(25, 3)),
+    columns=list("ABC"),
+    index=pd.date_range("2021-01-01", "2021-01-02", freq="H"),
+)
+
+
+def test_stack():
+
+    ts_row_wise = stack_timeseries(ts_column_wise)
+
+    assert ts_row_wise.columns == ts_row_wise_cols
+
+
+def test_unstack():
+
+    ts_row_wise = stack_timeseries(ts_column_wise)
+
+    ts_row_wise_again = unstack_timeseries(ts_column_wise)
+
+    assert pd.testing.assert_frame_equal(ts_row_wise_again, ts_row_wise)
