bfcd366d15c82cdf0d71a94b4953b632807e6bd9
unknown <julius.meier@gmx.de>
2021-07-15 12:06:24 +0200

    Adapt conventional power plant data to scalar data template

--- scripts/prepare_conv_pp.py
+++ scripts/prepare_conv_pp.py
@@ -8,6 +8,8 @@ in_path2 : str
     path of input file with geodata of regions in Germany as .gpgk
 in_path3 : str
     path of input file with names of regions in Berlin and Brandenburg as .yaml
+in_path4 : str
+    path of template for scalar data as .csv
 out_path : str
     path of output file with prepared data as .csv
 
@@ -22,7 +24,7 @@ Description
 -------------
 The script filters the OPSD conventional power plant package for power plants
 in Berlin and Brandenburg. The retrieved data is stored in a new dataframe, aggregated and
-saved as a csv file.
+saved as a csv file in the format of the scalar data template.
 Only operating power plants are considered.
 """
 import sys
@@ -36,7 +38,8 @@ if __name__ == "__main__":
     in_path1 = sys.argv[1]  # path to OPSD data
     in_path2 = sys.argv[2]  # path to geopackage of german regions
     in_path3 = sys.argv[3]  # path to b3_regions.yaml
-    out_path = sys.argv[4]
+    in_path4 = sys.argv[4]  # path to _scalar_template.csv
+    out_path = sys.argv[5]
 
     pp_opsd_de = pd.read_csv(in_path1)
     pp_opsd_b3 = pp_opsd_de[pp_opsd_de.state.isin(["Brandenburg", "Berlin"])]
@@ -100,5 +103,33 @@ if __name__ == "__main__":
         level=["region", "energy_source", "technology", "chp"], inplace=True
     )
 
+    # change data format to _scalar_template
+    conv_scalars = b3_agg.melt(id_vars=["region", "energy_source", "technology"])
+
+    scalar_template = pd.read_csv(in_path4, delimiter=";")
+
+    scalar_template["id_scal"] = conv_scalars.index
+    scalar_template["scenario"] = "Status quo"
+    scalar_template["var_name"] = conv_scalars["variable"]
+    scalar_template["carrier"] = conv_scalars["energy_source"]
+    scalar_template["region"] = conv_scalars["region"]
+    scalar_template["tech"] = conv_scalars["technology"]
+    scalar_template["var_value"] = conv_scalars["value"]
+    scalar_template[
+        "reference"
+    ] = "https://doi.org/10.25832/conventional_power_plants/2020-10-01"
+    scalar_template[
+        "comment"
+    ] = "filename: conventional_power_plants_DE.csv, aggregated based on NUTS3 region"
+
+    unit_dict = {"chp": None, "capacity_net_el": "MW", "efficiency_estimate": None}
+
+    def set_unit(df, unit_dict):
+        for key, value in unit_dict.items():
+            print(key, value)
+            df.loc[df["var_name"] == key, "var_unit"] = value
+
+    set_unit(scalar_template, unit_dict)
+
     # export prepared conventional power plant data
-    b3_agg.to_csv(out_path, index=False)
+    scalar_template.to_csv(out_path, index=False)
