a57e4f1eb301841812800c948df59fa8a9c9e238
jnnr <32454596+jnnr@users.noreply.github.com>
2021-07-19 17:52:04 +0200

    Prepare data for plot

--- scripts/plot_scalar_resources.py
+++ scripts/plot_scalar_resources.py
@@ -27,19 +27,39 @@ if __name__ == "__main__":
         os.makedirs(target_dir)
 
     def load_scalars(path):
-        df = pd.read_csv(path, sep=";", index_col=0)
+        df = pd.read_csv(path, sep=",", index_col=0)
         return df
 
     # Load scalar data
     scalars = load_scalars(resources)
 
-    # TODO: filter
+    def unstack_scalars(df):
+        _df = df.copy()
+        _df = _df.unstack("var_name")
 
-    # TODO: pivot
+        return _df
+
+    scalars.set_index(["region", "carrier", "tech", "var_name"], inplace=True)
+    scalars = scalars["var_value"]
+
+    # There are duplicate entries for those capacities where we distinguish chp=yes/no
+    # TODO: Rethink this
+    scalars = scalars.loc[scalars.index.duplicated()]
+
+    # unstack
+    scalars_prepared = unstack_scalars(scalars)
+
+    # filter
+    scalars_prepared = scalars_prepared["capacity_net_el"]
+
+    # Set numeric datatype
+    scalars_prepared = scalars_prepared.astype(float)
 
     # plot
     fig, ax = plt.subplots()
 
-    scalars.plot.bar(ax=ax)
+    scalars_prepared.plot.bar(ax=ax)
+
+    plt.tight_layout()
 
     plt.savefig(target)
