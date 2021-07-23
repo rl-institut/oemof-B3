fe56ac36b2fb43656a158fb5387a618b11bb99ca
unknown <julius.meier@gmx.de>
2021-07-23 15:22:45 +0200

    Edit plot_scalars()

--- scripts/plot_joined_scalars.py
+++ scripts/plot_joined_scalars.py
@@ -25,7 +25,11 @@ h = "storage_losses"
 j = "flow_in_ch4"
 k = "summed_variable_costs_in_ch4"
 
+# User input
 var_name = q
+ylabel = "Test"
+regions = ["BB", "BE"] # BE_BB
+conv_number = None
 
 names_dict = {"BE-biomass-st":"Biomass ST","BB-biomass-st":"Biomass ST",
               "BE-electricity-curtailment":"El. curtailment","BB-electricity-curtailment":"Curtailment",
@@ -49,11 +53,11 @@ scalars = pd.read_csv(data_path)
 
 # select data with chosen var_name
 selected_scalar_data = scalars[scalars['var_name'] == var_name]
-selected_scalar_data = selected_scalar_data[selected_scalar_data["region"] == "BB"]
+selected_scalar_data = selected_scalar_data[selected_scalar_data["region"].isin(regions)]
 
 print(tabulate(selected_scalar_data, headers='keys', tablefmt='psql'))
 
-def prepare_scalar_data(df, colors_odict, names_dict):
+def prepare_scalar_data(df, colors_odict, names_dict, conv_number = conv_number):
     # rename
     df = df.copy()
     df["name"] = df["name"].replace(names_dict)
@@ -65,45 +69,66 @@ def prepare_scalar_data(df, colors_odict, names_dict):
     #pv.dropna(axis = "index", how="all", inplace=True)
     #pv.dropna(axis = "columns", how="all", inplace=True)
 
-    # define ordering and assign color
+    # define ordering and use concrete_order as keys for colors_odict in plot_scalars()
     order = list(colors_odict)
-    colors=[]
     concrete_order=[]
     for i in order:
         if i not in df_pivot.columns:
             continue
-        colors.append(colors_odict[i])
         concrete_order.append(i)
     df_pivot = df_pivot[concrete_order]
 
-    return df_pivot, colors
+    # convert data to SI-Units
+    if conv_number is not None:
+        df_pivot *= conv_number
 
+    return df_pivot, concrete_order
 
-def plot_scalars(df, colors, unit_dict, var_name):
-
+def plot_scalars(
+    df, var_name, ylabel, color_keys, unit_dict=unit_dict, fontsize=14
+):
+    plt.rcParams.update({"font.size": fontsize})
     # Create figure with a subplot for each scenario with a relative width
     # proportionate to the number of regions
     scenarios = df.index.levels[0]
     nplots = scenarios.size
     plots_width_ratios = [df.xs(scenario).index.size for scenario in scenarios]
-    fig, axes = plt.subplots(nrows=1, ncols=nplots, sharey=True, figsize=(12, 6),
-                             gridspec_kw=dict(width_ratios=plots_width_ratios, wspace=0))
+    fig, axes = plt.subplots(
+        nrows=1,
+        ncols=nplots,
+        sharey=True,
+        figsize=(12, 6),
+        gridspec_kw=dict(width_ratios=plots_width_ratios, wspace=0),
+    )
+    # make sure axes is in iterable object
+    if isinstance(axes, np.ndarray):
+        axes_list = axes
+    else:
+        axes_list = []
+        axes_list.append(axes)
 
     # Loop through array of axes to create grouped bar chart for each scenario
     alpha = 0.3  # used for grid lines, bottom spine and separation lines between scenarios
-    for scenario, ax in zip(scenarios, axes):
+    for scenario, ax in zip(scenarios, axes_list):
         # df.xs() Return cross-section from the Series/DataFrame. Here: return data of one scenario.
         df_scenario = df.xs(scenario)
-        # apply EngFormatter
+        # apply EngFormatter if power is plotted
         if unit_dict[var_name] == "W":
             ax = plots.eng_format(ax, "W")
 
         # Create bar chart with grid lines and no spines except bottom one
-        df_scenario.plot.bar(ax=ax, legend=None, zorder=2, color=colors, width=0.8, stacked=False)
-        ax.grid(axis='y', zorder=1, color='black', alpha=alpha)
-        for spine in ['top', 'left', 'right']:
+        df_scenario.plot.bar(
+            ax=ax,
+            legend=None,
+            zorder=2,
+            color=[colors_odict[key] for key in color_keys],
+            width=0.8,
+            stacked=False,
+        )
+        ax.grid(axis="y", zorder=1, color="black", alpha=alpha)
+        for spine in ["top", "left", "right"]:
             ax.spines[spine].set_visible(False)
-        ax.spines['bottom'].set_alpha(alpha)
+        ax.spines["bottom"].set_alpha(alpha)
 
         # Set and place x labels for scenarios
         ax.set_xlabel(scenario)
@@ -111,11 +136,13 @@ def plot_scalars(df, colors, unit_dict, var_name):
 
         # Format major tick labels for region names: note that long names are
         # rewritten on two lines.
-        ticklabels = [name.replace(' ', '\n') if len(name) > 10 else name
-                      for name in df.xs(scenario).index]
-        ax.set_xticklabels(ticklabels, rotation=0, ha='center')
+        ticklabels = [
+            name.replace(" ", "\n") if len(name) > 10 else name
+            for name in df.xs(scenario).index
+        ]
+        ax.set_xticklabels(ticklabels, rotation=0, ha="center")
 
-        ax.tick_params(axis='both', length=0, pad=7)
+        ax.tick_params(axis="both", length=0, pad=7)
 
         # Set and format minor tick marks for separation lines between scenarios: note
         # that except for the first subplot, only the right tick mark is drawn to avoid
@@ -125,15 +152,15 @@ def plot_scalars(df, colors, unit_dict, var_name):
             ax.set_xticks([*ax.get_xlim()], minor=True)
         else:
             ax.set_xticks([ax.get_xlim()[1]], minor=True)
-        ax.tick_params(which='minor', length=55, width=0.8, color=[0, 0, 0, alpha])
+        ax.tick_params(which="minor", length=55, width=0.8, color=[0, 0, 0, alpha])
 
     # Add legend using the labels and handles from the last subplot
-    fig.legend(*ax.get_legend_handles_labels(), frameon=False,)
+    fig.legend(*ax.get_legend_handles_labels(), frameon=True, framealpha=1)
 
-    fig.suptitle(var_name + ' by scenario and region', y=1.02, size=14)
+    plt.ylabel(ylabel=ylabel)
 
     plt.tight_layout()
-    plt.savefig(var_name + ".png",bbox_inches='tight')
+    plt.savefig("joined_scalars.png", bbox_inches="tight")
 
 
 # prepare data
@@ -141,5 +168,5 @@ def plot_scalars(df, colors, unit_dict, var_name):
 prepared_scalar_data, colors = prepare_scalar_data(df=selected_scalar_data, colors_odict=colors_odict, names_dict=names_dict)
 print(tabulate(prepared_scalar_data, headers='keys', tablefmt='psql'))
 # plot data
-plot_scalars(df = prepared_scalar_data, colors=colors, unit_dict=unit_dict, var_name=var_name)
+plot_scalars(df = prepared_scalar_data, color_keys=colors, unit_dict=unit_dict, var_name=var_name, ylabel=ylabel)
 plt.show()
