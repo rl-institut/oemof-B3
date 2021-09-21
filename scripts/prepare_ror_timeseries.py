ed6d180d0e26c41ccf3c3a66daad92a898126a31
unknown <julius.meier@gmx.de>
2021-09-21 13:09:21 +0200

    Add template for ror timeseries preparation script

---
+++ scripts/prepare_ror_timeseries.py
@@ -0,0 +1,23 @@
+r"""
+Inputs
+-------
+
+
+Outputs
+---------
+
+
+Description
+-------------
+
+"""
+import sys
+import oemof_b3.tools.data_processing as dp
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+    #ror_raw = sys.argv[1]  # path to raw ror data
+
+    ror_raw = r"\\FS01\RL-Institut\04_Projekte\305_UMAS_Gasspeicher\03-Projektinhalte\Daten\raw\time_series\DIW_Hydro_availability.csv"
+
