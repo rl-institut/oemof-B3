b3e56070f8176b5b68ba389f6ba6af9d08398f19
jnnr <32454596+jnnr@users.noreply.github.com>
2021-10-20 12:20:35 +0200

    Fill NaNs with base scenario values

--- scripts/prepare_scalars.py
+++ scripts/prepare_scalars.py
@@ -20,9 +20,46 @@ The script performs the following steps to prepare scalar data for parametrizati
 """
 import sys
 
+import pandas as pd
+
 from oemof.tools.economics import annuity
 
 from oemof_b3.tools.data_processing import ScalarProcessor, load_b3_scalars
+
+
+def fill_na(df):
+    key = "scenario"
+
+    value = "None"
+
+    _df = df.copy()
+
+    # save index and columns before resetting index
+    id_names = _df.index.names
+
+    columns = _df.columns
+
+    _df.reset_index(inplace=True)
+
+    # separate data where NaNs should be filled and base
+    df_fill_na = _df.loc[_df[key] != value]
+
+    base = _df.loc[_df[key] == value]
+
+    # merge data on the columns of the data to update
+    df_merged = df_fill_na.drop(columns, 1).merge(base.drop(key, 1), "left")
+
+    # update dataframe NaNs
+    df_fill_na.update(df_merged)
+
+    # combine the filled data with the base data
+    df_fill_na = pd.concat([df_fill_na, base])
+
+    # set index as before
+    df_fill_na = df_fill_na.set_index(id_names)
+
+    return df_fill_na
+
 
 if __name__ == "__main__":
     in_path = sys.argv[1]  # path to raw scalar data
@@ -33,6 +70,9 @@ if __name__ == "__main__":
     sc = ScalarProcessor(df)
 
     invest_data = sc.get_unstacked_var(["overnight_cost", "lifetime"])
+
+    # if some value is None in some scenario key, use the values from Base scenario to fill NaNs
+    invest_data = fill_na(invest_data)
 
     wacc = sc.get_unstacked_var("wacc").iloc[0, 0]
 
