e87a9f0ef5793b212efcf1f34529e5d01cc0e1c5
jnnr <32454596+jnnr@users.noreply.github.com>
2022-01-06 12:09:44 +0100

    Define constants for optimization options

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -10,30 +10,32 @@ from oemof.tabular import datapackage  # noqa
 from oemof.tabular.facades import TYPEMAP
 
 
+SOLVER = "cbc"
+RECEIVE_DUALS = True
+FILENAME_METADATA = "datapackage.json"
+
+
 if __name__ == "__main__":
     preprocessed = sys.argv[1]
 
     optimized = sys.argv[2]
 
-    filename_metadata = "datapackage.json"
-
-    solver = "cbc"
-
     if not os.path.exists(optimized):
         os.mkdir(optimized)
 
     es = EnergySystem.from_datapackage(
-        os.path.join(preprocessed, filename_metadata), attributemap={}, typemap=TYPEMAP
+        os.path.join(preprocessed, FILENAME_METADATA), attributemap={}, typemap=TYPEMAP
     )
 
     # create model from energy system (this is just oemof.solph)
     m = Model(es)
 
     # tell the model to get the dual variables when solving
-    m.receive_duals()
+    if RECEIVE_DUALS:
+        m.receive_duals()
 
     # solve
-    m.solve(solver=solver)
+    m.solve(solver=SOLVER)
 
     # get the results from the the solved model(still oemof.solph)
     es.meta_results = processing.meta_results(m)
