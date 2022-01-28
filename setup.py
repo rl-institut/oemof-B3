3b9e8fdc733a59153c2d739abd114f58a2cf0069
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2022-01-28 15:48:26 +0100

    added a (dirty) fix for #114 by forcing installation order

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ import os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+extras = {
+    "standard": ["pandas", "rtree", "pyyaml", "snakemake", "sphinx-rtd-theme>=0.5.1"],
+    "dev": ["pytest", "black==20.8b1", "coverage", "flake8", "geopandas"],
+}
+
+extras["full"] = list(set(extras["standard"] + extras["dev"]))
+
 
 setup(
     name="oemof-B3",
@@ -15,14 +22,7 @@ setup(
     long_description=read("README.md"),
     packages=["oemof_b3"],
     install_requires=[
-        "pandas",
-        "rtree",
-        "pyyaml",
-        "snakemake",
         "oemoflex @ git+https://git@github.com/rl-institut/oemoflex@dev#egg=oemoflex",
-        "sphinx-rtd-theme>=0.5.1",
     ],
-    extras_require={
-        "dev": ["pytest", "black==20.8b1", "coverage", "flake8", "geopandas"]
-    },
+    extras_require=extras,
 )
