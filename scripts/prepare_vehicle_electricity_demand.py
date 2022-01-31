43cce91c3f1a9e87bafa209faef5f64f470daa3d
SabineH <sabine.haas@rl-institut.de>
2022-01-31 12:08:45 +0100

    Add strategy balanced for home+work during specific hours of day

--- scripts/prepare_vehicle_electricity_demand.py
+++ scripts/prepare_vehicle_electricity_demand.py
@@ -19,8 +19,10 @@ Description
 -------------
 This script prepares electric vehicle charging demand time series for the regions Berlin
 and Brandenburg. The time series have been created before with simBEV
-(https://github.com/rl-institut/simbev). The charging strategy is "greedy", i.e. batteries are
-charged with maximum power until they are fully charged or removed.
+(https://github.com/rl-institut/simbev). The charging strategy of simBEV data is "greedy", i.e.
+batteries are charged with maximum power until they are fully charged or removed. This scripts
+applies a charging strategy "balanced" for the profiles "home" and "work" during specific hours (see
+global variables).
 
 """
 
@@ -33,24 +35,36 @@ import oemof_b3.tools.data_processing as dp
 TS_VAR_UNIT = "None"
 TS_SOURCE = "created with simBEV"
 TS_COMMENT = "https://github.com/rl-institut/simbev"
+HOME_START = "15:00"  # start charging strategy "balanced" for home profile
+HOME_END = "05:00"  # end charging strategy "balanced" for home profile
+WORK_START = "06:00"  # start charging strategy "balanced" for work profile
+WORK_END = "14:00"  # end charging strategy "balanced" for work profile
 
 
-def prepare_vehicle_time_series(input_dir):
+def prepare_vehicle_time_series(input_dir, balanced=True):
     r"""
     Prepares and formats electric vehicle charging demand time series for regions 'B' and 'BB'.
+
+    The profiles are resampled from 15-min to hourly time steps. If `balanced` is True the profiles
+    "work" and "home" are smoothed between `HOME_START`, `HOME_END` and `WORK_START`, `WORK_END`
+    respectively with charging strategy "balanced".
+    The charging demand profile is then divided by the total demand.
 
     Parameters
     ----------
     input_dir : str
         Path to directory where csv files containing electric vehicle charging demand time series
         are placed
+    balanced : bool
+        If True profiles "work" and "home" are smoothed with charging strategy "balanced".
+        Default: True
 
     Returns
     -------
     ts_prepared : pd.DataFrame
         Contains electric vehicle charging demand time series in the format of time series template
         of oemof-B3
-s
+
     """
     # initialize data frame
     df = pd.DataFrame()
@@ -64,23 +78,34 @@ s
 
         # read data from file, copy and superfluous drop last time step
         ts_raw = pd.read_csv(
-            path, index_col=1, sep=";", decimal=",", parse_dates=True, thousands=".",
+            path,
+            index_col=1,
+            sep=";",
+            decimal=",",
+            parse_dates=True,
+            thousands=".",
         ).drop(columns=["Unnamed: 0"], axis=1)
         ts = ts_raw[ts_raw.index.year == year]
 
         # resample (15 min to hourly), unit is kW
         hourly_ts = ts.resample("H").mean()
 
-        # normalize with total electricity demand
-        hourly_ts_norm = hourly_ts / hourly_ts.sum()
+        if balanced:
+            # smooth work and home profiles as they have high peaks (~strategy balanced)
+            hourly_ts = balance_profiles(df=hourly_ts)
 
         # only keep column "sum CS power" (sum of power demand at all charging stations)
-        ts_df = pd.DataFrame(hourly_ts_norm["sum CS power"]).rename(
+        ts_total_demand = pd.DataFrame(hourly_ts["sum CS power"]).rename(
             columns={"sum CS power": f"ts_{year}"}
         )
 
+        # divide by total electricity demand of vehicles
+        ts_total_norm = ts_total_demand / ts_total_demand.sum()
+
         # stack time series and add region
-        ts_stacked = dp.stack_timeseries(ts_df).rename(columns={"var_name": "scenario"})
+        ts_stacked = dp.stack_timeseries(ts_total_norm).rename(
+            columns={"var_name": "scenario"}
+        )
         ts_stacked.loc[:, "region"] = region
 
         # add to `df`
@@ -100,6 +125,50 @@ s
     ts_prepared.loc[:, "comment"] = TS_COMMENT
 
     return ts_prepared
+
+
+def balance_profiles(df):
+    r"""
+    Smoothes profiles "work" and "home" of `df` between specific hours (see global variables).
+
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        Contains vehicle charging profiles.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        Vehicle charging profiles with smoothed "home" and "work" profiles and adapted total
+        charging demand.
+    """
+
+    def balanced_between_hours(ts, start, end):
+        """Apply charging strategy 'balanced' between two hours of the day."""
+        # calculate average between start and end
+        average = ts.between_time(start, end).mean()
+        # set average for times between start and end
+        ts.loc[ts.between_time(start, end).index] = average
+        return ts
+
+    df["sum UC work"] = df.groupby(df.index.date)["sum UC work"].apply(
+        lambda x: balanced_between_hours(ts=x, start=WORK_START, end=WORK_END)
+    )
+
+    # for home: determine which hours of the day should belong to next day
+    temp = (
+        pd.DataFrame(df.index)["time"].apply(lambda x: 0 if x.hour < 12 else 1).values
+    )
+    df["temp"] = df.index.dayofyear + temp
+
+    df["sum UC home"] = df.groupby(df["temp"])["sum UC home"].apply(
+        lambda x: balanced_between_hours(ts=x, start=HOME_START, end=HOME_END)
+    )
+
+    # get total charging df after balancing
+    df["sum CS power"] = df.drop("sum CS power", axis=1).sum(axis=1)
+
+    return df
 
 
 if __name__ == "__main__":
