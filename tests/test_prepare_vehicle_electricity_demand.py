289235187ea13f144b356a1d92c9c887a7a129c9
SabineH <sabine.haas@rl-institut.de>
2022-02-14 13:29:08 +0100

    Test that smoothed ts sums up to 1 and use teardown_function

--- tests/test_prepare_vehicle_electricity_demand.py
+++ tests/test_prepare_vehicle_electricity_demand.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from pandas.util.testing import assert_frame_equal, assert_series_equal
 
 from scripts.prepare_vehicle_charging_demand import prepare_vehicle_charging_demand
@@ -10,19 +11,24 @@ vehicle_files_test_dir = os.path.join(test_dir, "_files", "test_vehicle_ts")
 filename_results = os.path.join(
     test_dir, "_files", "oemof_b3_resources_timeseries_elec_vehicle_demand.csv"
 )
-
-
-def test_prepare_vehicle_time_series():
-    df = prepare_vehicle_charging_demand(input_dir=vehicle_files_test_dir)
-
-    # temporarily save df and load again for correct dtypes
-    temp_filename = os.path.join(
+temp_filename = os.path.join(
         test_dir,
         "_files",
         "temp.csv",
     )
+
+
+def _save_and_reload_ts(df):
     save_df(df, temp_filename)
     df = load_b3_timeseries(temp_filename)
+    return df
+
+
+def test_prepare_vehicle_charging_demand():
+    df = prepare_vehicle_charging_demand(input_dir=vehicle_files_test_dir, balanced=True)
+
+    # temporarily save df and load again for correct dtypes
+    df = _save_and_reload_ts(df)
 
     # read expected results
     df_exp = load_b3_timeseries(filename_results)
@@ -30,5 +36,15 @@ def test_prepare_vehicle_time_series():
     assert_frame_equal(df, df_exp)
     assert_series_equal(df["series"], df_exp["series"])
 
-    # delete temporary file
+
+def test_prepare_vehicle_charging_demand_smoothing():
+    df = prepare_vehicle_charging_demand(input_dir=vehicle_files_test_dir, balanced=True)
+
+    # temporarily save df and load again for correct dtypes
+    df = _save_and_reload_ts(df)
+
+    assert sum(df["series"].iloc[0]) == float(1)
+
+
+def teardown_function():
     os.remove(temp_filename)
