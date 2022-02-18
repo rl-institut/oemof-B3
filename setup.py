3fee03f9ff1c9c9f59add18629602ac90e4222f5
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2022-02-18 11:29:58 +0100

    Undone unintended config merge

--- setup.py
+++ setup.py
@@ -9,14 +9,8 @@ def read(fname):
 
 
 extras = {
-    "standard": [
-        "pandas",
-        "rtree",
-        "pyyaml",
-        "snakemake",
-        "dynaconf",
-    ],
-    "dev": ["pytest", "black==20.8b1", "coverage", "flake8", "geopandas", "pre-commit"],
+    "standard": ["pandas", "rtree", "pyyaml", "snakemake"],
+    "dev": ["pytest", "black==20.8b1", "coverage", "flake8", "geopandas"],
 }
 
 extras["full"] = list(set(extras["standard"] + extras["dev"]))
