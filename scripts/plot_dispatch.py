4f2d37ede0917d720353614352b1807488be7eec
monika <monika@orl25.de>
2022-03-02 16:20:28 +0100

    Solve flake8 E501

--- scripts/plot_dispatch.py
+++ scripts/plot_dispatch.py
@@ -6,7 +6,8 @@ postprocessed : str
     ``results/{scenario}/postprocessed/``: path to directory which contains the input data which
     can be plotted
 plotted : str
-    ``results/{scenario}/plotted/dispatch/``: path where a new directory is created and the plots are saved
+    ``results/{scenario}/plotted/dispatch/``: path where a new directory is created and
+    the plots are saved
 
 Outputs
 ---------
