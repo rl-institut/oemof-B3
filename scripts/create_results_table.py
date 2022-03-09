954cca5aa3fa881bfbb1aa34db3542f4a70b56c4
jnnr <32454596+jnnr@users.noreply.github.com>
2022-03-09 16:33:13 +0100

    Move functions to top

--- scripts/create_results_table.py
+++ scripts/create_results_table.py
@@ -33,56 +33,58 @@ def unstack_var_name(df):
     return _df.unstack("var_name")
 
 
+def create_production_table(scalars, carrier):
+    VAR_NAMES = [
+        "capacity",
+        f"invest_out_{carrier}",
+        f"flow_out_{carrier}",
+        "storage_capacity",
+        "invest",
+        "loss",
+    ]
+
+    df = scalars.copy()
+
+    # df = dp.aggregate_scalars(df, "region")
+
+    df = dp.filter_df(df, "var_name", VAR_NAMES)
+
+    df = unstack_var_name(df).loc[:, "var_value"]
+
+    df = df.loc[~df[f"flow_out_{carrier}"].isna()]
+
+    df.index = df.index.droplevel(["name", "scenario", "type"])
+
+    df.loc[:, "FLH"] = df.loc[:, f"flow_out_{carrier}"] / df.loc[
+        :, ["capacity", "invest"]
+    ].sum(axis=1)
+
+    return df
+
+
+def create_demand_table(scalars):
+    df = scalars.copy()
+
+    var_name = "flow_in_"
+
+    # df = dp.aggregate_scalars(df, "region")
+
+    df = df.loc[df["var_name"].str.contains(var_name)]
+
+    df = dp.filter_df(df, "type", ["excess", "load"])
+
+    df = df.set_index(["region", "carrier", "tech"])
+
+    df = df.loc[:, ["var_name", "var_value"]]
+
+    return df
+
+
 if __name__ == "__main__":
     in_path = sys.argv[1]  # input data
     out_path = sys.argv[2]
 
     scalars = pd.read_csv(os.path.join(in_path, "scalars.csv"))
-
-    def create_production_table(scalars, carrier):
-        VAR_NAMES = [
-            "capacity",
-            f"invest_out_{carrier}",
-            f"flow_out_{carrier}",
-            "storage_capacity",
-            "invest",
-            "loss",
-        ]
-
-        df = scalars.copy()
-
-        # df = dp.aggregate_scalars(df, "region")
-
-        df = dp.filter_df(df, "var_name", VAR_NAMES)
-
-        df = unstack_var_name(df).loc[:, "var_value"]
-
-        df = df.loc[~df[f"flow_out_{carrier}"].isna()]
-
-        df.index = df.index.droplevel(["name", "scenario", "type"])
-
-        df.loc[:, "FLH"] = df.loc[:, f"flow_out_{carrier}"] / df.loc[
-            :, ["capacity", "invest"]
-        ].sum(axis=1)
-
-        return df
-
-    def create_demand_table(scalars):
-        df = scalars.copy()
-
-        var_name = "flow_in_"
-
-        # df = dp.aggregate_scalars(df, "region")
-
-        df = df.loc[df["var_name"].str.contains(var_name)]
-
-        df = dp.filter_df(df, "type", ["excess", "load"])
-
-        df = df.set_index(["region", "carrier", "tech"])
-
-        df = df.loc[:, ["var_name", "var_value"]]
-
-        return df
 
     if not os.path.exists(out_path):
         os.makedirs(out_path)
