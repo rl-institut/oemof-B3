cc0fb1bd92a8678c3f3e4eac18cf81b32a098574
MaGering <marie-claire.gering@rl-institut.de>
2022-03-14 19:31:57 +0100

    Bump version number

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Reiner Lemoine Institut'
 author = 'Reiner Lemoine Institut'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2dev'
 
 
 # -- General configuration ---------------------------------------------------
