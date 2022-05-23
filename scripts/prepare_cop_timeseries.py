f13887978f2add5ab1bfb2806433ee885e4abac8
MaGering <marie-claire.gering@rl-institut.de>
2022-05-23 15:52:48 +0200

    Add out_path to docstring of prepare_cop_timeseries module

--- scripts/prepare_cop_timeseries.py
+++ scripts/prepare_cop_timeseries.py
@@ -6,6 +6,9 @@ in_path1 : str
     ``raw/weatherdata``: path of input directory with weather data
 in_path2 : str
     ``raw/scalars/demands.csv``: path of scalar data as .csv
+out_path : str
+    ``results/_resources/ts_efficiency_heatpump_small.csv``: path of output file with timeseries
+    data as .csv
 logfile : str
     ``logs/{scenario}.log``: path to logfile
 
