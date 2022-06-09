761c3f29504e5682718abb8dd8fa503a49fa4798
monika <monika@orl25.de>
2022-06-09 18:59:34 +0200

    Add comments

--- scripts/plot_scalar_results.py
+++ scripts/plot_scalar_results.py
@@ -735,10 +735,13 @@ if __name__ == "__main__":
         plot.prepared_scalar_data = plot.prepared_scalar_data.filter(
             items=["var_value"]
         )
+
+        # Remember index to apply it after unstacking
         index = plot.prepared_scalar_data.index.get_level_values(0).unique()
         # Unstack prepared and filtered data regarding carriers
         plot.prepared_scalar_data = plot.prepared_scalar_data.unstack("var_name")
 
+        # Reindex to keep previous scenario order
         plot.prepared_scalar_data = plot.prepared_scalar_data.reindex(index)
 
         # Get names of data's columns
