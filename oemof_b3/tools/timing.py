f29db472f5bc61d8dedd413589b6ed412a577f38
jnnr <32454596+jnnr@users.noreply.github.com>
2022-08-10 17:07:41 +0200

    Adapt reporting of time

--- oemof_b3/tools/timing.py
+++ oemof_b3/tools/timing.py
@@ -1,6 +1,7 @@
 r"""
 A Timer class, adapted from https://github.com/realpython/codetiming
 """
+import datetime
 import time
 
 
@@ -19,7 +20,9 @@ class Timer:
         elapsed_time = time.perf_counter() - self._start_time
         self._start_time = None
         if self.logger:
-            self.logger(self.text + f" Elapsed time: {elapsed_time:0.4f} seconds")
+            self.logger(
+                self.text + f" Elapsed time: {datetime.timedelta(seconds=elapsed_time)}"
+            )
 
     def __enter__(self):
         """Start a new timer as a context manager"""
