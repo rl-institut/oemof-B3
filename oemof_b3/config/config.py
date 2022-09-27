2718b43f29ef80c8cfa7a9e6989a2651914e25db
MaGering <marie-claire.gering@rl-institut.de>
2022-09-27 15:45:29 +0200

    Add dirty fix of dynaconf.Dynaconf.get Error

--- oemof_b3/config/config.py
+++ oemof_b3/config/config.py
@@ -5,45 +5,47 @@ from dynaconf import Dynaconf
 from oemoflex.tools.helpers import load_yaml
 
 CONFIG_PATH = pathlib.Path(__file__).parent
+THIS_FILE_PATH = pathlib.Path(__file__)
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
     settings_files=[CONFIG_PATH / "settings.yaml", CONFIG_PATH / ".secrets.yaml"],
 )
 
+if "readthedocs" not in str(THIS_FILE_PATH):
 
-class LevelFilter(logging.Filter):
-    def __init__(self, level):
-        self.level = level
-        super(LevelFilter, self).__init__()
+    class LevelFilter(logging.Filter):
+        def __init__(self, level):
+            self.level = level
+            super(LevelFilter, self).__init__()
 
-    def filter(self, record):
-        return record.levelno != self.level
+        def filter(self, record):
+            return record.levelno != self.level
 
-
-DEBUG = settings.get("DEBUG", False)
-LOGGING_LEVEL = settings.get("LOGGING_LEVEL", logging.DEBUG if DEBUG else logging.INFO)
-LOGGING_FOLDER = "logs"
-
-root_logger = logging.getLogger()
-root_logger.setLevel(LOGGING_LEVEL)
-
-stream_formatter = logging.Formatter("%(levelname)s - %(message)s")
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(stream_formatter)
-stream_handler.addFilter(LevelFilter(logging.ERROR))
-root_logger.addHandler(stream_handler)
-
-
-def add_snake_logger(logfile, rulename):
-    logger = logging.getLogger(rulename)
-    file_formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    DEBUG = settings.get("DEBUG", False)
+    LOGGING_LEVEL = settings.get(
+        "LOGGING_LEVEL", logging.DEBUG if DEBUG else logging.INFO
     )
-    handler = logging.FileHandler(logfile)
-    handler.setFormatter(file_formatter)
-    logger.addHandler(handler)
-    return logger
+    LOGGING_FOLDER = "logs"
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(LOGGING_LEVEL)
+
+    stream_formatter = logging.Formatter("%(levelname)s - %(message)s")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(stream_formatter)
+    stream_handler.addFilter(LevelFilter(logging.ERROR))
+    root_logger.addHandler(stream_handler)
+
+    def add_snake_logger(logfile, rulename):
+        logger = logging.getLogger(rulename)
+        file_formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler = logging.FileHandler(logfile)
+        handler.setFormatter(file_formatter)
+        logger.addHandler(handler)
+        return logger
 
 
 LABELS = load_yaml(CONFIG_PATH / "labels" / f"{settings.labels}.yml")
