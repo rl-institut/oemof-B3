651b7f2f1766916c83a3f2f49820db863e5e2d0e
jnnr <32454596+jnnr@users.noreply.github.com>
2022-10-10 12:25:09 +0200

    Provide schema in module

--- oemof_b3/schema/__init__.py
+++ oemof_b3/schema/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 import json
+import pandas as pd
 
 
 def load_json_to_dict(filepath):
@@ -9,5 +10,13 @@ def load_json_to_dict(filepath):
 
 
 here = os.path.dirname(__file__)
+
+SCHEMA_SCAL = pd.read_csv(
+    os.path.join(here, "scalars.csv"), index_col=0, delimiter=";"
+)
+
+SCHEMA_TS = pd.read_csv(
+    os.path.join(here, "timeseries.csv"), index_col=0, delimiter=";"
+)
 
 oemetadata_template = load_json_to_dict(os.path.join(here, "oemetadata.json"))
