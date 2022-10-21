af3af1211c00cd751743eab3adb9528499cbe4f5
MaGering <marie-claire.gering@rl-institut.de>
2022-10-21 15:33:34 +0200

    Correct optimize docstring

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -22,7 +22,7 @@ Given an EnergyDataPackage, this script creates an oemof.solph.EnergySystem and 
 oemof.solph.Model, which is optimized.
 The following constraints are added:
 - `emission_limit`: maximum amount of emissions
-- `equate_flows_by_keyword`: electricty-gas relation is set (electricity/gas = factor).
+- `equate_flows_by_keyword`: electricity-gas relation is set (electricity/gas = factor).
     This constraint is only added if 'electricity_gas_relation' is added to the scalars.
     To use this constraint you need to copy
     [`equate_flows.py`](https://github.com/oemof/oemof-solph/blob/features/equate-flows/src/oemof/solph/constraints/equate_variables.py)
