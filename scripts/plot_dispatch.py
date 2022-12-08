97122a7e39c43945ab755b50590b491c5b05254f
MaGering <52790556+MaGering@users.noreply.github.com>
2022-12-08 15:19:01 +0100

    Update scripts/plot_dispatch.py
    Co-authored-by: jnnr <32454596+jnnr@users.noreply.github.com>

--- scripts/plot_dispatch.py
+++ scripts/plot_dispatch.py
@@ -218,11 +218,8 @@ def get_df_for_aggregation(list_with_dfs):
         Result DataFrame with concatenated DataFrames
 
     """
-    df_concatenated = pd.DataFrame()
-
-    for df_in_list in list_with_dfs:
-        df_concatenated = pd.concat(
-            [df_concatenated, df_in_list], axis=0, ignore_index=True
+    df_concatenated = pd.concat(
+            list_with_dfs, axis=0, ignore_index=True
         )
 
     return df_concatenated
