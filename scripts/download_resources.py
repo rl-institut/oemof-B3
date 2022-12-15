a2e47cc8860ee6a52251492a1ed118fed5ab8cff
jnnr <32454596+jnnr@users.noreply.github.com>
2022-12-15 11:35:17 +0100

    Accept list of resources in script to download them

--- scripts/download_resources.py
+++ scripts/download_resources.py
@@ -1,19 +1,19 @@
-from collections import namedtuple
-from oemof_b3.config import config
+import sys
+import pathlib
+
 from oemof_b3.tools import oep
 
 OEP_SCHEMA = "model_draft"
-
-RESOURCES_DIR = config.ROOT_DIR / "results" / "_resources"
-Resource = namedtuple("Resource", ("name", "tablename"))
-RESOURCES = (Resource("test", "sedos_extended_scalars"),)  # TODO: Fill in all resources
-
-
-def download_resources():
-    for resource in RESOURCES:
-        output_filename = RESOURCES_DIR / f"{resource.name}.csv"
-        oep.download_table_from_OEP(output_filename, resource.tablename, OEP_SCHEMA)
+NAME_PREFIX = "resources"
 
 
 if __name__ == "__main__":
-    download_resources()
+    target_paths = [pathlib.Path(path) for path in sys.argv[1:]]
+
+    for filepath in target_paths:
+
+        tablename = "_".join([NAME_PREFIX, filepath.stem])
+
+        print(f"Attempting to download {tablename}")
+
+        oep.download_table_from_OEP(filepath, tablename, OEP_SCHEMA)
