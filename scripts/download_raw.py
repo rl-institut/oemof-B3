435fe3a9ad1c0dd873ff4453c3a380185bef2546
jnnr <32454596+jnnr@users.noreply.github.com>
2023-02-15 15:57:04 +0100

    Unzip zip file

--- scripts/download_raw.py
+++ scripts/download_raw.py
@@ -2,14 +2,23 @@ import sys
 from pathlib import Path
 
 from urllib import request
+from zipfile import ZipFile
 
 
 def download_from_zenodo(url, target_directory):
     request.urlretrieve(url, target_directory)
 
 
+def unzip(zip_filepath, destination):
+    with ZipFile(zip_filepath, 'r') as zObject:
+        zObject.extractall(path=destination)
+
+
 if __name__ == "__main__":
     url = sys.argv[1]
     raw = Path(sys.argv[2])
+    zip_filepath = raw / "oemof-B3-raw-data.zip"
 
-    download_from_zenodo(url, raw)
+    download_from_zenodo(url, zip_filepath)
+
+    unzip(zip_filepath, raw)
