df3cbc54225a06d809cee60e19c0e5a4d20c2236
MaGering <marie-claire.gering@rl-institut.de>
2023-02-16 16:55:36 +0100

    Change version number from 0.0.2dev to 0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Reiner Lemoine Institut'
 author = 'Reiner Lemoine Institut'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2dev'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
