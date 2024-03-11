644d3579fef3639d5dacde47d733c89d307f2231
MaGering <marie-claire.gering@rl-institut.de>
2024-03-11 18:29:07 +0100

    Change version to release version v0.0.4 which should appear on main branch

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Reiner Lemoine Institut'
 author = 'Reiner Lemoine Institut'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4dev'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
