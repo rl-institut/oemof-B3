41fc55f9782786afc7a08ce506aff8b5c0e43bd0
MaGering <marie-claire.gering@rl-institut.de>
2024-03-11 18:42:52 +0100

    Increase version number to v0.0.5dev as perparation for dev

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Reiner Lemoine Institut'
 author = 'Reiner Lemoine Institut'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.5dev'
 
 
 # -- General configuration ---------------------------------------------------
