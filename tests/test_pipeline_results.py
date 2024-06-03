3997415aaaa1986dca64f09cfd5c515934fe2f2a
MaGering <marie-claire.gering@rl-institut.de>
2024-06-03 17:24:50 +0200

    Update result rules to be testes in test_pipeline_results

--- tests/test_pipeline_results.py
+++ tests/test_pipeline_results.py
@@ -10,19 +10,33 @@ scenarios = [
     "2050-80-gas_moreCH4",
 ]
 
+scenario_groups = [
+    "examples",
+    "all-scenarios",
+    "all-custom-order",
+    "all-optimized",
+]
+
 for scenario in scenarios:
 
     output_rule_list = [
         "results/" + scenario + "/preprocessed",
         "results/" + scenario + "/optimized",
         "results/" + scenario + "/postprocessed",
-        # "results/joined_scenarios/{scenario_group}/joined",
         "results/" + scenario + "/b3_results/data",
         "results/" + scenario + "/tables",
-        # "results/joined_scenarios/{scenario_group}/joined_tables",
         "results/" + scenario + "/plotted/dispatch",
         "results/" + scenario + "/plotted/storage_level",
         "results/" + scenario + "/plotted/scalars",
-        # "results/joined_scenarios/{scenario_group}/joined_plotted",
         "results/" + scenario + "/report",
     ]
+
+for scenario_group in scenario_groups:
+    output_rule_list = [
+        "results/joined_scenarios/" + scenario_group + "/joined",
+        "results/joined_scenarios/" + scenario_group + "/joined_tables",
+        "results/joined_scenarios/" + scenario_group + "/joined_plotted",
+    ]
+
+# snakemake -j1 TODO: SN hinzufügen
+output_rule_list = ["results/_resources/RE_potential"]
