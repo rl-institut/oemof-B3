8d6ce35eef623b0c36dd2b164826e9c58779ca6f
Stefanie Nguyen <stefanie.nguyen@rl-institut.de>
2025-01-10 16:51:47 +0100

    Remove rules which are tested in pipeline_dir_output_test

--- tests/test_pipeline_resources_tables.py
+++ tests/test_pipeline_resources_tables.py
@@ -26,30 +26,15 @@ os.chdir(target_path)
 
 output_rule_list = [
     ["results/_resources/scal_conv_pp.csv"],
-    ["results/_resources/ts_feedin.csv"],
-    ["results/_resources/ts_load_electricity.csv"],
-    ["results/_resources/ts_load_electricity_vehicles.csv"],
-    ["results/_resources/scal_costs_efficiencies.csv"],
-    ["results/_resources/ts_efficiency_heatpump_small.csv"],
     # snakemake -j1 plot_conv_pp_scalars
     ["results/_resources/plots/scal_conv_pp-capacity_net_el.png"],
     ["results/_tables/technical_and_cost_assumptions_2050-base.csv"],
-    ["results/_resources/scal_load_heat.csv", "results/_resources/ts_load_heat.csv"],
     # snakemake -j1 process_re_potential
     [
         "results/_resources/scal_power_potential_wind_pv.csv",
         "results/_tables/potential_wind_pv_kreise.csv",
     ],
     # snakemake -j1 download_resources
-    [
-        "results/_resources/ts_feedin.csv",
-        "results/_resources/ts_load_electricity.csv",
-        "results/_resources/ts_load_electricity_vehicles.csv",
-        "results/_resources/scal_costs_efficiencies.csv",
-        "results/_resources/ts_efficiency_heatpump_small.csv",
-        "results/_resources/scal_load_heat.csv",
-        "results/_resources/ts_load_heat.csv",
-    ],
 ]
 
 
